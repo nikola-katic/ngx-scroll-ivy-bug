9bede3941709f1105ead1cded7e261a313fbf6a6
nikola <nikola.katic91@gmail.com>
2019-09-15 16:41:04 +0200

    Ivy bug

---
+++ ngx-scrollbar-bug/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgScrollbarModule } from 'ngx-scrollbar';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgScrollbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
